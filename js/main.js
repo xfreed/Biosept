47efa700a97af99dfc83f256259cde2d8ecf1913
Michael Kunynets <skilledaligator@gmail.com>
2019-06-16 17:47:29 +0300

    Update

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+    $( document ).ready(function() {
+      $('.burger').click(function(){
+        $('body').toggleClass('active');
+      });
+      $('.mobile-menu li a').click(function(){
+        $('body').removeClass('active');
+      });
+});
+   $(window).scroll(function(){
+        var the_top = $(document).scrollTop();
+    if (the_top > 83) {
+        $('.top-menu').addClass('fixed');
+    }
+    else {
+        $('.top-menu').removeClass('fixed');
+    }
+    })
