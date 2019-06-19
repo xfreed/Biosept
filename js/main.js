6e157df8601c138862387148cb75d90f864df0aa
Michael Kunynets <skilledaligator@gmail.com>
2019-06-19 22:46:55 +0300

    update

--- js/main.js
+++ js/main.js
@@ -49,3 +49,21 @@ $('#contactForm').submit(function () {
   $('#hiding').addClass('button-clicked');
   return false;
 });
+// ScrollBack
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700 
+    && $(document).width()>500) {
+    document.getElementById("ScrollBack").style.display = "block";
+  } else {
+    document.getElementById("ScrollBack").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
